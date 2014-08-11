e81b22f75914794c090b3b87f2ab12c8f2112f04
Per Greisen <pgreisen@gmail.com>
2014-08-11 09:18:15 -0700

    Change to the change chain ID file incorporated

---
+++ change_chainID_ligand.py
@@ -0,0 +1,30 @@
+import os,shutil,sys,subprocess
+
+def main():
+    
+    pdbfile = sys.argv[1]
+    
+    newchainid = sys.argv[2]
+
+    filename = open(pdbfile,'r')
+
+    tmpfile = open('tmp.pdb','w')
+    
+    for line in filename:
+
+        if line[0:4] == 'HETA':
+            newline = str(line[0:20])+' '+str(newchainid)+str(line[22:])
+            tmpfile.write(newline)
+        else:
+            tmpfile.write(line)
+    tmpfile.close()
+    filename.close()
+
+    mvfl = 'mv tmp.pdb '+str(pdbfile)
+
+    subprocess.Popen(mvfl,shell=True).wait()
+
+    
+if __name__ == "__main__":
+    main()
+
