fc8e69ac7e8024a397fd0ee3baa32cbc54d0a295
pgreisen <pgreisen@gmail.com>
2014-08-21 16:26:03 -0700

    add files

--- dev/permutation/combinations.py
+++ dev/permutation/combinations.py
@@ -1,8 +1,10 @@
 def combinations(string):
      yield ''
+     # (0, thing[0]), (1, thing[1]), (2, thing[2]), and so forth
      for i, d in enumerate(string):
-         for comb in combinations(string[i+1:]):
-             yield d + comb
+          print "i,d",i,d
+          for comb in combinations(string[i+1:]):
+               yield d + comb
 
 
 a= combinations("1234")
