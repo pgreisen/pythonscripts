169cf46dcabb06322eed7dddc1ed9870703afbf2
pgreisen <pgreisen@gmail.com>
2014-09-02 09:00:05 -0700

    Add script to split pdb files by NMR

---
+++ split_nmr_into_multiple_pdbs.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python2
+# Author Firas Khatib
+from os import popen
+import sys
+
+input = open(sys.argv[1], 'r').readlines()
+
+model = -1
+for i in input:
+    line = i.split()
+
+    if len(line) > 1:
+        if line[0] == "MODEL":
+            model = int(line[1])
+            output = open(sys.argv[1]+".model."+str(model)+".pdb", 'w')
+
+        if model >= 0:
+            output.write(i)
