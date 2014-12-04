1268aaf6b19159229905cca9ec3221f1bf25d4b4
pgreisen <pgreisen@gmail.com>
2014-12-04 15:02:39 -0800

    Initialization of variable, lig_coor = [], in class getligandpositionmatchermain

--- dev/matcher_per_position/getligandpositionmatchermain.py
+++ dev/matcher_per_position/getligandpositionmatchermain.py
@@ -59,7 +59,6 @@ class getligandpositionmatchermain:
 
         parser.add_argument("--tmp_argument", dest="tmp_argument",help="The is a variable to insure you are not overridding the current file when you run multiple runs in the same directory", default="")
 
-
         input_var = parser.parse_args()
     
         gp = GetLigandPositions()
@@ -70,6 +69,9 @@ class getligandpositionmatchermain:
 
         # Get ligand coordinates
         new_positions = []
+
+
+        lig_coor = []
 
         if(input_var.ligandnames != None):
 
@@ -82,6 +84,8 @@ class getligandpositionmatchermain:
                     # assert 0 == 1
                     # lig_coor = gp.get_ligandatom_aromatic_coordinates(p_pdbfile,tmplig)
                     # Need to append here to get the same type as for none aromatic systems
+
+
                     lig_coor.append(gp.get_ligandatom_aromatic_coordinates(p_pdbfile,tmplig))
                     # print "The new coordinates of the ligand are ",lig_coor
                     # Ok only one vector
