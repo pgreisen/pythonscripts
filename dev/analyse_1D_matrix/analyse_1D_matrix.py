5b4eff4d243baf0bd7dcd7e9d5118c274a860df7
pgreisen <pgreisen@gmail.com>
2015-04-16 17:48:16 -0700

    1D analysis

--- dev/analyse_1D_matrix/analyse_1D_matrix.py
+++ dev/analyse_1D_matrix/analyse_1D_matrix.py
@@ -4,13 +4,11 @@ from collections import defaultdict
 from collections import OrderedDict
 import os,shutil,sys,argparse
 
-class Analyse2D:
+class Analyse1D:
 
     def __init__(self):
         self.scores = defaultdict(list)
         self.scores_position_two = {}
-
-
 
         self.aa = ['ALA','ARG','ASN','ASP','CYS','GLN','GLU','GLY','HIS','ILE','LEU','LYS','MET','PHE','PRO','SER','THR','TRP','TYR','VAL']
         self.aa_single_letter = ['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V']
@@ -22,7 +20,7 @@ class Analyse2D:
         self.score_term = "total_score"
         self.score_term_position = 0
         # A_D_resfile_scores
-        self.baseline_value = -552.660
+        self.baseline_value = -534.412
 
 
     def get_sorted_hashtable(self, hashtable):
@@ -40,54 +38,34 @@ class Analyse2D:
                     for t in range( len(tmp_line) ):
                         if (tmp_line[t] == self.score_term):
                             self.score_term_position = t
+
                     tmp_variable = False
                 elif( tmp_variable == False):
-
                     aa_tmp = filename.split('_')
                     tmp_value = round(float( tmp_line[self.score_term_position] ) - self.baseline_value,3)
-                    self.scores[ str(aa_tmp[0]) ].append( (aa_tmp[1], str( tmp_value )  ) )
-
-                    # works
-                    #self.scores[ str(aa_tmp[0]) ].append( (aa_tmp[1], tmp_line[self.score_term_position] - self.baseline_value)  )
-
-                    ##print  aa_tmp[1] ,  tmp_line[self.score_term_position]
-                    ##self.scores[ str(aa_tmp[0]) ].append( scores_position_two[ aa_tmp[1] ] = tmp_line[self.score_term_position]  )
-
-                    #self.scores_position_two = { aa_tmp[1] : tmp_line[self.score_term_position] }
-                    #tmp_variable2 = True
-
-
-                    #elif( tmp_variable2 == True):
-                    #self.scores[ str(aa_tmp[0]) ].append( self.scores_position_two )
+                    self.scores[ str(aa_tmp[0]) ].append( (aa_tmp[0], str( tmp_value )  ) )
 
 
     def write_matrix_to_file(self):
 
-        with open("2Dscan.csv",'w') as f:
-            f.write("AA(203/233),")
+        with open("1Dscan.csv",'w') as f:
+            f.write("267,")
             for key in self.aa_single_letter:
                 f.write(key+",")
 
             f.write("\n")
-            #assert 1 == 0
-            # print self.scores
-
             for key in self.aa_single_letter:
 
-                f.write(key+",")
-                ##print key
-                ##assert 1 == 0
                 tmp_dic = self.scores[ key ]
 
-                for key in self.aa_single_letter:
+                print tmp_dic
 
-                    for i in range(len(tmp_dic) ):
 
-                        if(key == tmp_dic[i][0]):
-                            # continue
-                            # print key, tmp_dic[i]
-                            f.write(tmp_dic[i][1]+",")
-                f.write("\n")
+                for i in range(len(tmp_dic) ):
+                
+                    if(key == tmp_dic[i][0]):
+                        f.write(tmp_dic[i][1]+",")
+            f.write("\n")
 
 
     def main(self):
@@ -97,7 +75,6 @@ class Analyse2D:
         # get the initial rosetta design as input
         parser.add_argument("-s","--score_term", dest="score_term", help="Which score term to analyse (Default total_score )", default="total_score")
 
-        # parser.add_argument("-b", "--bundles", dest="helical_bundle", help="Four chains helical bundle with four chains is set to true", action="store_true", default=False )
 
         path = "./"
 
@@ -112,10 +89,9 @@ class Analyse2D:
             if( os.path.isfile(fl) and fl.endswith("scores") ):
                 self.set_matrix( fl )
 
-
         self.write_matrix_to_file()
 
 
 if __name__ == "__main__":
-   run = Analyse2D()
+   run = Analyse1D()
    run.main()
