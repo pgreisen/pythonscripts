f46b0f6d21f05eebef884020981e2fe7b888431b
pgreisen <pgreisen@gmail.com>
2015-04-24 20:03:54 -0700

    get position files from lab

--- dev/GetPositionFiles/get_pdbfile_from_lab.py
+++ dev/GetPositionFiles/get_pdbfile_from_lab.py
@@ -38,7 +38,7 @@ class GetPositionFiles:
                 for fl in files:
                     tmpfile = fl.split('.')
                     # import pdb; pdb.set_trace()
-                    if( int( tmpfile[0][-1] ) < self.cutoff_positionfile ):
+                    if( int( tmpfile[0][-1] ) < self.cutoff_positionfile and fl.endswith(".pos")):
 
                         print fl
 
