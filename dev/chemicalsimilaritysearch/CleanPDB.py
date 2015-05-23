e8f8ce4a3968d4a14ebde9031b2aa5312524541f
pgreisen <pgreisen@gmail.com>
2015-05-22 21:41:12 -0700

    clean PDB

--- dev/chemicalsimilaritysearch/CleanPDB.py
+++ dev/chemicalsimilaritysearch/CleanPDB.py
@@ -15,8 +15,6 @@ class CleanPDB:
             for line in f:
                 if(line[0:4] == "ATOM" or line[0:4] == "HETA"):
 
-                    import pdb; pdb.set_trace()
-
                     self.pdbfile_chains[line[21:22]].append(line)
 
         return self.pdbfile_chains
