2392de8556d519af92c2d1fb91995d52e48ee2e9
pgreisen <pgreisen@gmail.com>
2015-07-16 22:14:00 -0700

    Fixing cluster

--- dev/cluster/kmeans_rmsd_17072015.py
+++ dev/cluster/kmeans_rmsd_17072015.py
@@ -9,19 +9,16 @@ def substract_mean(lst,key,centroids):
      min_value = 10000
      list_of_values = []
      for value in lst:
-          #print type(value), type(centroids[key][0])
-          print value - centroids[key][0]
           new_value = abs(value - centroids[key][0])
           if( new_value < min_value):
                min_value = value
-     print "KEY is ",key
-     print "CCCC",min_value
-     print "DDDD",centroids[key][0]
      print "The closest point in cluster :",key
      print "RMSD = ", min_value
-     if(min_value in lst):
-          print min_value
-          print "Found"
+     # todo
+     # insert assert statement that value is present
+     #if(min_value in lst):
+     #     #print min_value
+     #     #print "Found"
 
 ##########################
 sort_cluster_points = defaultdict(list)
@@ -48,36 +45,30 @@ with open(tmpfile,'r') as f:
 ###############################################################
 data = vstack(data)
 centroids,labels = kmeans2( data, size_of_clusters )
-#print labels
-print centroids
-#print "###################################"
-#print len(data), len(labels), len(centroids)
 
-
+#print "Labels are: ", labels
 
 
 for i in range( len(labels ) ):
      key = labels[i]
      sort_cluster_points[key].append(float(data[i][0]))
      
-
 for key in sort_cluster_points:
+     #print "Input key is: ", key
      substract_mean(sort_cluster_points[key],key,centroids)
-
-assert 1 ==0 
 
 # assign each sample to a cluster
 idx,_ = vq(data,centroids)
-###help(kmeans)
-###help(vq)
 # some plotting using numpy's logical indexing
 plot(data[idx==0,0],data[idx==0,1],'ob', data[idx==1,0],data[idx==1,1],'or')
 plot(centroids[:,0],centroids[:,1],'sg',markersize=8)
 
-show()
-
+#show()
+#
+'''
 center_of_cluster = centroids[:,0]
 for tm in center_of_cluster:
-     print "###: ",tm
+     #print "###: ",tm
      if( tm in clusters.keys() ):
           print cluster[keys]
+'''
