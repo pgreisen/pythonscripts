85cfe0fe5ced893b1c8b2d018caffbd9ad7790b8
Per Jr. Greisen <pgreisen@gmail.com>
2018-02-11 10:54:27 +0100

    Script to concatenate Rosetta scorefiles and get the lowest score

---
+++ develop/concatenate_scorefiles_and_plot_score/plotscore.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import glob
+import argparse,csv
+import operator
+import seaborn as sns
+
+
+class ScoreMultiplefiles:
+
+
+    def __init__(self):
+        self.scorex = ""
+        self.scorey = ""
+        self.scorez = ""
+
+        self.df = None
+
+
+    def get_dataframe(self):
+        df_ = []
+        for scorefile in glob.glob('*.sc'):
+            df_.append(pd.read_csv(scorefile,delim_whitespace=True,skiprows=1) )
+        return pd.concat(df_)
+
+    def plot_data(self,x,y,dataframe):
+        sns_plot = sns.lmplot(x, y, data=dataframe, fit_reg=False)
+        sns_plot.savefig("output.png")
+
+
+    def main(self):
+
+        parser = argparse.ArgumentParser(description="Concatenate scorefiles and plot two value specified and dumb top X number")
+
+        # get the initial rosetta design as input
+        parser.add_argument("-x", "--scorex", dest="scorex", help="")
+        parser.add_argument("-y", "--scorey", dest="scorey", help="")
+        parser.add_argument("-z", "--scorez", dest="scorez", help="")
+
+
+        args_dict = vars( parser.parse_args() )
+        for item in args_dict:
+            setattr(self, item, args_dict[item])
+
+        df  = self.get_dataframe()
+        stat =  df[self.scorey].describe()
+        # print stat.index, type(stat),stat[u'25%'],stat
+        print "The 25% value is ", stat[u'25%']
+
+        print df[self.scorez].describe()
+
+        # filter values
+        df = df[ ( df[self.scorey] < stat[u'25%']  ) & ( df[self.scorez] < 3  )   ]
+        # sort dataframe
+        df.sort_values(by=self.scorex,ascending=False, inplace=True)
+        self.plot_data(self.scorex,self.scorey,df)
+        print df.head(1)
+        print df['description'].head(1)
+
+        with open('lowest_pdb.txt','w') as f:
+            f.write(list(df['description'].head(1))[0])
+
+
+
+if __name__ == "__main__":
+   run = ScoreMultiplefiles()
+   run.main()
+
+
+
+
