19040dbd44151e8d45bdad8e96a0375fc01c2250
Per Jr. Greisen <pgreisen@gmail.com>
2019-01-02 19:54:40 +0100

    Updating scripts and debugging

--- run_bioinformatics_programs/run_pssm_nvlv.sh
+++ run_bioinformatics_programs/run_pssm_nvlv.sh
@@ -1,6 +1,27 @@
-# copy files to 
-
-
+######################################
+# copy script to update ec2 instance - assume it is ubuntu for now
+wget https://raw.githubusercontent.com/pgreisen/pythonscripts/master/bashscripts/update_ec2_aws/update_ec2_ubuntu_aws_no_python.sh
+sh update_ec2_aws/update_ec2_ubuntu_aws_no_python.sh;
+######################################
+# next copy executables from S3 bucket
+aws s3 cp s3://enevolvcomputationalbiology/databases/uniref90files.zip .
+aws s3 cp s3://tempfilespssm/ . --include="*.zip"
+######################################
+# setup for pssm run
 path=/home/ubuntu/ncbi-blast-2.7.1+/bin
 database=/home/ubuntu/uniref90
-$path/psiblast -query $1 -db $database/uniref90_w_index.fasta -out_pssm my_protein.ckp -evalue 0.01 -out_ascii_pssm ascii_mtx_file -out output_file -num_iterations 3 
+######################################
+# unzip file for pssm
+unzip uniref90files.zip;
+rm uniref90files.zip;
+unzip *zip;
+rm *zip;
+for i in *.fasta;
+do
+    dst=${i%.fasta}\_pssm;
+    mkdir $dst;
+    mv $i $dst;
+    cd $dst;
+    $path/psiblast -query $i -db $database/uniref90_w_index.fasta -out_pssm my_protein.ckp -evalue 0.01 -out_ascii_pssm ascii_mtx_file -out output_file -num_iterations 3 > pssm_logfile & echo "done";
+    cd ..;
+done
