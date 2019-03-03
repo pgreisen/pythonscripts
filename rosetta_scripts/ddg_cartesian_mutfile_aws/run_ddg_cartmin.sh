ff18f38ba6f2eae21fa6792a17c0b18f8a92a0d9
Per Jr. Greisen <pgreisen@gmail.com>
2019-03-03 19:31:10 +0100

    qsub script fixed

--- rosetta_scripts/ddg_cartesian_mutfile_aws/run_ddg_cartmin.sh
+++ rosetta_scripts/ddg_cartesian_mutfile_aws/run_ddg_cartmin.sh
@@ -48,7 +48,7 @@ mv ../1.pdb .;
 ~/relax.static.linuxgccrelease -s 1.pdb -use_input_sc -constrain_relax_to_start_coords -ignore_unrecognized_res -nstruct 20 -relax:coord_constrain_sidechains -relax:cartesian -score:weights ref2015_cart -relax:min_type lbfgs_armijo_nonmonotone -relax:script cart2.script -database ~/database
 #####
 pdbfile=`sort -n -k2 score.sc | awk '{print $NF}' | head -n 1`;
-cp $pdbfile.pdb ../lowest.pdb;
+cp $pdbfile.pdb ~/lowest.pdb;
 cd ..;
 # We loop over all resfile and setup directories for 
 # their calculations
@@ -63,6 +63,6 @@ do
         resf=$j;
     done
     ln -s $resf mutfile;
-    nohup qsub ~/qsub_run_ddg.sh & echo "done";
+    nohup qsub -v pdbfile=~/lowest.pdb ~/qsub_run_ddg.sh & echo "done";
     cd ..;
 done
