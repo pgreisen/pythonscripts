d135098733b70ca6c059d45e91a5c62d4a0f19d0
Per Jr. Greisen <pgreisen@gmail.com>
2019-03-05 17:29:40 +0100

    qsub script fixed

--- rosetta_scripts/ddg_cartesian_mutfile_aws/qsub_run_ddg.sh
+++ rosetta_scripts/ddg_cartesian_mutfile_aws/qsub_run_ddg.sh
@@ -2,4 +2,4 @@
 #PBS -N JobName
 #PBS -l walltime=72:00:00
 #PBS -l mem=10gb
-~/cartesian_ddg.static.linuxgccrelease -database ~/database -s $pth/lowest.pdb -ddg:mut_file $pth/mutfile -ddg:iterations 10 -ddg::cartesian -ddg::dump_pdbs false -ddg:bbnbr 1 -fa_max_dis 9.0 -score:weights ref2015_cart -out:path:score $pth -unmute all
+~/cartesian_ddg.static.linuxgccrelease -database ~/database -s ./lowest.pdb -ddg:mut_file ./mutfile -ddg:iterations 10 -ddg::cartesian -ddg::dump_pdbs false -ddg:bbnbr 1 -fa_max_dis 9.0 -score:weights ref2015_cart -out:path:score $pth -unmute all
