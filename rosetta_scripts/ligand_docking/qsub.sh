4463374b0cdf096cc01918cab389d5b8fd573c44
Per Jr. Greisen <pgreisen@gmail.com>
2019-07-02 14:42:30 -0400

    Fixing path for executable

--- rosetta_scripts/ligand_docking/qsub.sh
+++ rosetta_scripts/ligand_docking/qsub.sh
@@ -6,4 +6,5 @@
 #PBS -j oe
 rosetta_pth=~/;
 rosetta_db=~/database;
-$rosetta_pth/rosetta_scripts.static.linuxgccrelease -database $rosetta_db -extra_res_fa $parameters -parser:protocol dock.xml -in:file:s $native -in:file:native $native -out:prefix $prefix -nstruct 50 -out:file:silent_struct_type binary -out:file:silent $prefix\_silent.out -out:file:scorefile $prefix\_dock.sc -packing:ex1 -packing:ex2 -out:prefix $prefix @flags -parser:script_vars x=$x -parser:script_vars y=$y -parser:script_vars z=$z
+$rosetta_pth/rosetta_scripts.static.linuxgccrelease -database $rosetta_db -extra_res_fa $parameters -parser:protocol dock.xml -in:file:s $native -in:file:native $native -out:prefix $prefix -nstruct 50 -out:file:scorefile $prefix\_dock.sc -packing:ex1 -packing:ex2 -out:prefix $prefix @flags -parser:script_vars x=$x -parser:script_vars y=$y -parser:script_vars z=$z
+# -out:file:silent_struct_type binary -out:file:silent $prefix\_silent.out
