2192bd2aa6eaa173dec18df75309893d2fae0825
Per Jr. Greisen <pgreisen@gmail.com>
2019-07-09 15:13:35 -0400

    Setting path correctly for runing psiblast

---
+++ develop/PlotlyScripts/load_libs.py
@@ -0,0 +1,8 @@
+import plotly
+from plotly.graph_objs import Scatter, Layout
+import plotly
+import plotly.offline as offline
+import plotly.graph_objs as go
+import plotly.plotly as py
+import plotly.figure_factory as ff
+plotly.offline.init_notebook_mode(connected=False)
---
+++ develop/PlotlyScripts/scatterplot.py
@@ -0,0 +1,24 @@
+def get_scatter_plot(dataframe,xlabel,ylabel,description='description',plottitle="Default"):
+    data = []
+    data.append(go.Scatter(x=dataframe[xlabel],y=dataframe[ylabel],mode='markers',\
+                           text=dataframe[description], marker=dict(size=10)))
+    layout = go.Layout(title=plottitle,xaxis=dict(title=xlabel,
+                                                  titlefont=dict(
+                                          family='Courier New, monospace',
+                                          size=18,
+                                          color='#7f7f7f')),
+                           
+                                                     yaxis=dict(
+                               title=ylabel,
+                               titlefont=dict(
+                                   family='Courier New, monospace',
+                                   size=18,
+                                   color='#7f7f7f')))
+    fig = go.Figure(data=data, layout=layout)
+    return fig
+
+var1= "NAME"
+fig = get_scatter_plot(df, x, y,'description',var1 )
+offline.iplot(fig, filename='unrooted-tree')
+offline.plot(fig, image='png', filename=var1+".html"+, image_filename=var1,auto_open=False)
+
