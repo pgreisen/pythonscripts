b446767d7157ef7627956b80748bb1257fc8977f
Per Jr. Greisen <pgreisen@gmail.com>
2019-09-13 15:22:28 -0400

    fix hm script

--- jupyternotebook/GremlinAnalysis/run_nb_to_html.sh
+++ jupyternotebook/GremlinAnalysis/run_nb_to_html.sh
@@ -1,2 +1,2 @@
-papermill /Users/pgreisen/pythonscripts/jupyternotebook/GremlinAnalysis/20190308_Synlogics_gremlin_analysis.ipynb gremlin_analysis.ipynb 
+papermill /Users/pgreisen/pythonscripts/jupyternotebook/GremlinAnalysis/20190308_gremlin_analysis.ipynb gremlin_analysis.ipynb 
 jupyter nbconvert --to html gremlin_analysis.ipynb
--- rosetta_scripts/cm_rosetta_homologymodels/getfiles_and_setup_rosettaCM_nvlv.sh
+++ rosetta_scripts/cm_rosetta_homologymodels/getfiles_and_setup_rosettaCM_nvlv.sh
@@ -12,16 +12,14 @@ mkdir rosetta_bin;
 cd rosetta_bin;
 # test if files have already been copied
 if [ ! -f "partial_thread.static.linuxgccrelease" ]; then
-    aws s3 cp s3://enevolvcomputationalbiology/programs/partial_thread.static.linuxgccrelease.tgz .
+    aws s3 cp s3://enevolvcomputationalbiology/programs/partial_thread.static.linuxgccrelease.tgz .;
+    tar zxf partial_thread.static.linuxgccrelease.tgz;
 fi
 if [ ! -f "rosetta_scripts.static.linuxgccrelease" ]; then
-    aws s3 cp s3://enevolvcomputationalbiology/programs/rosetta_scripts.static.linuxgccrelease.tgz .
+    aws s3 cp s3://enevolvcomputationalbiology/programs/rosetta_scripts.static.linuxgccrelease.tgz .;
+    tar zxf rosetta_scripts.static.linuxgccrelease.tgz;
 fi
-for i in *tgz;
-do
-    tar zxf $i;
-    rm $i;
-done
+rm *tgz;
 cd ../..;
 if [ ! -d "database" ]; then
     aws s3 cp s3://enevolvcomputationalbiology/programs/database.tgz .;
