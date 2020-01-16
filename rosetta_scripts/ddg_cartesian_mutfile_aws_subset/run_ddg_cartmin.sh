5f2c4cba4fde8c310156bd47872f4f12f85b8cb8
Per Greisen <p.greisen@enevolv.com>
2020-01-16 15:52:12 -0500

    Transfering scripts

--- rosetta_scripts/ddg_cartesian_mutfile_aws_subset/run_ddg_cartmin.sh
+++ rosetta_scripts/ddg_cartesian_mutfile_aws_subset/run_ddg_cartmin.sh
@@ -62,6 +62,7 @@ endrepeat" > cart2.script;
 mv ../1.pdb .;
 # start the cartesian minimization before substitution
 nohup ~/relax.static.linuxgccrelease -s 1.pdb -use_input_sc -constrain_relax_to_start_coords -ignore_unrecognized_res -nstruct 20 -relax:coord_constrain_sidechains -relax:cartesian -score:weights ref2015_cart -relax:min_type lbfgs_armijo_nonmonotone -relax:script cart2.script -database ~/database;
+wait;
 #####
 pdbfile=`sort -n -k2 score.sc | awk '{print $NF}' | head -n 1`;
 cp $pdbfile.pdb ~/lowest.pdb;
