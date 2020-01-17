34d75a227f47ce8f76d6fd680efd8295bfb52f51
Per Greisen <p.greisen@enevolv.com>
2020-01-17 13:47:53 -0500

    Parser

--- rosetta_scripts/ddg_cartesian_mutfile_aws_subset/qsub_run_ddg.sh
+++ rosetta_scripts/ddg_cartesian_mutfile_aws_subset/qsub_run_ddg.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -f
-~/cartesian_ddg.static.linuxgccrelease -database ~/database -s $pth/lowest.pdb -ddg:mut_file $pth/$mutfile -ddg:iterations 20 -ddg::cartesian -ddg::dump_pdbs false -fa_max_dis 9.0 -score:weights ref2015_cart -out:path:score $pth -unmute all -ex1 -ex2 ddg:bbnbr 1
+~/cartesian_ddg.static.linuxgccrelease -database ~/database -s $pth/lowest.pdb -ddg:mut_file $pth/$mutfile -ddg:iterations 20 -ddg::cartesian -ddg::dump_pdbs false -fa_max_dis 9.0 -score:weights ref2015_cart -out:path:score $pth -unmute all -ex1 -ex2 -ddg:bbnbr 1
