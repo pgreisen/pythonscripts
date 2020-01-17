db0804407fae810ff421f0cf47b490b8ed14c8f9
Per Greisen <p.greisen@enevolv.com>
2020-01-17 14:50:01 -0500

    bash-script to update ubuntu or centos machines

--- bashscripts/update_ec2_aws/update_ec2.sh
+++ bashscripts/update_ec2_aws/update_ec2.sh
@@ -1,5 +1,5 @@
 dist=`awk -F= '/^NAME/{print $2}' /etc/os-release`;
-if [ "$dist" == "Ubuntu" ]; then
+if [ "$dist" == "\"Ubuntu\"" ]; then
   echo "ubuntu"
   sudo apt update -y
   DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
@@ -14,7 +14,7 @@ if [ "$dist" == "Ubuntu" ]; then
   sudo apt-get install -y zlib1g-dev 
   sudo apt-get install -y scons 
 fi
-if [ "$dist" == "Amazon Linux AMI" ]; then
+if [ "$dist" == "\"Amazon Linux AMI\"" ]; then
     echo "Amazon Linux AMI";
     sudo yum update -y
     sudo yum install awscli emacs unzip zip scons -y
